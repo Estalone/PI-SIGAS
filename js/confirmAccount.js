1ec94978799eeadd8799b45e5329c1b36027a2c6
CyberknightMK <cyberknightmk@gmail.com>
2026-09-25 11:16:33 -0300

    Update confirmAccount.js
    Fixed!

--- js/confirmAccount.js
+++ js/confirmAccount.js
@@ -79,15 +79,17 @@ formConfirm.addEventListener("submit",async(e)=>{
 			}else if(response.status===500){
 				throw new Error("Erro interno no servidor (500). Tente novamente mais tarde.");
 			}else if(response.status===400){
-				throw new Error("O usuário e/ou o E-Mail informado já está cadastrado no sistema.");
+				throw new Error(`Token ou E-Mail inválido: ${response.status}.`);
+			}else if(response.status===406){
+				throw new Error(`Token já utilizado ou E-Mail não encontrado: ${response.status}.`);
 			}else{
-				throw new Error(`Erro na requisição: Code ${response.status}.`);
+				throw new Error(`Erro na requisição: código ${response.status}.`);
 			}
 		}
 
 		const data=await response.json();
 		exibirMensagem(
-			`Registro realizado com sucesso! Título: ${data.title}`,
+			`Confirmação realizada com sucesso: ${data.title}.`,
 			"sucesso"
 		);
 	}catch(err){
