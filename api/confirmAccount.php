d32eb4fa56a3d2b68bc49f3984a8a74e51135918
CyberknightMK <cyberknightmk@gmail.com>
2026-09-25 11:17:56 -0300

    Update confirmAccount.php
    Fixed!

--- api/confirmAccount.php
+++ api/confirmAccount.php
@@ -20,27 +20,29 @@ $errors=[];
 
 // Validação dos campos
 if (empty($token)){
-    $errors["token"]="O token é obrigatório.";
+	http_response_code(400);
+	$errors["token"]="O token é obrigatório.";
 }
 
 $token=htmlspecialchars($token,ENT_QUOTES,"UTF-8");
 
 if (empty($email)) {
-    $errors["email"] = "Email é obrigatório.";
+	http_response_code(400);
+	$errors["email"] = "E-Mail é obrigatório.";
 } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    $errors["email"] = "Informe um e-mail válido.";
+	http_response_code(400);
+	$errors["email"] = "Informe um endereço de E-Mail válido.";
 }
 
 if(!empty($errors)){
-  // Une todas as menssagens de erro
-  $mensagemErro = implode(" ", $errors);
+	// Une todas as menssagens de erro
+	$mensagemErro = implode(" ", $errors);
 
-  http_response_code(400);
-    echo json_encode([
-        "success" => false,
-        "mensagem" => "Verifique os dados informados: ".$mensagemErro
-    ]);
-    exit;
+	echo json_encode([
+		"success" => false,
+		"mensagem" => "Verifique os dados informados: ".$mensagemErro
+	]);
+	exit;
 }
 
 // Prepara e executa a inserção com PDO
@@ -53,14 +55,21 @@ try{
 		"token"=>$token
 	]);
 
-	if($result){
+	if($result->numrows>0){
 		http_response_code(201);
 		echo json_encode([
 			"success"	=> true,
 			"mensagem"	=> "Conta confirmada com sucesso!"
 		]);
 		exit();
-	}
+	}else{
+		http_response_code(406);
+		echo json_encode([
+			"success"	=> false,
+			"mensagem"	=> "Token inválido ou E-Mail não encontrado! Verifique os dados, se a conta já está ativa ou tente ativar novamente."
+		]);
+		exit();
+    }
 }catch(PDOException $e){
 	http_response_code(500);
 	echo json_encode(["success" => false, "error" => "Erro interno no servidor."]);
